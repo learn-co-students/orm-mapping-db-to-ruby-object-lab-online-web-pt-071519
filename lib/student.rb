ce33daa74322240d000b0972ceb53c6676ea457f
Alouiscious Media <info@reneesoul.com>
2019-09-27 22:03:27 -0700

    git lab even

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,14 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
@@ -12,7 +18,80 @@ class Student
 
   def self.find_by_name(name)
     # find the student in the database given a name
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE name = ? 
+      LIMIT 1
+    SQL
+    
     # return a new instance of the Student class
+    @query_student = 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_9
+    # returns an array of all students in grades 9
+    sql = <<-SQL 
+      SELECT * 
+      FROM students 
+      WHERE grade = 9
+    SQL
+    
+    grade_9 = DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+
+  def self.students_below_12th_grade
+  #returns an array of all students in grades 11 or below
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade <= 11
+    SQL
+
+    grade_11_or_below = DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.all
+    # returns all student instances from the db
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10
+  # returns an array of the first X students in grade 10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY id ASC
+    SQL
+
+    first_X_10 = DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.first_student_in_grade_10
+  # returns the first student in grade 10
+
+  end
+
+  def self.all_students_in_grade_X
+  # returns an array of all students in a given grade X
+
   end
   
   def save
